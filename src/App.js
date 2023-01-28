74dce0663841b4dcb2ad0026a9c977a3adb834ad
MikeSanDev <mjs070594@gmail.com>
2023-01-27 22:26:36 -0600

    updated pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="About" element={<About />} />
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
