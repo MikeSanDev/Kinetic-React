fd62694cc0c42576177287567b5d64c417f4e439
MikeSanDev <mjs070594@gmail.com>
2023-02-03 00:48:26 -0600

    survey editss

--- src/pages/Survey.js
+++ src/pages/Survey.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavbarComponent from '../components/NavbarComponent';
 import Footer from '../components/Footer';
+import axios from 'axios'
 
 function Survey() {
     return (
         <>
-            <div>
-                <NavbarComponent />
-                <Footer />
+
+            <NavbarComponent />
+            <div className="survey-container">
+
+                <br></br>
+                <h1>Survey</h1>
             </div>
+
+
+            <Footer />
+
         </>
     )
 }
