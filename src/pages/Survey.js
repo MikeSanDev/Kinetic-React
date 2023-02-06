a3772fcaa144ca04cd532b9ba786ffc13704d92e
MikeSanDev <mjs070594@gmail.com>
2023-02-05 22:19:35 -0600

    new

--- src/pages/Survey.js
+++ src/pages/Survey.js
@@ -12,7 +12,7 @@ function Survey() {
     // form states 
     const [email, setEmail] = useState('');
     const [question1, setQuestion1] = useState('');
-    const [age, setAge] = useState(0);
+    const [age, setAge] = useState('');
     const [question3, setQuestion3] = useState('');
     const [question4, setQuestion4] = useState('');
     const [question5, setQuestion5] = useState('');
@@ -87,9 +87,9 @@ function Survey() {
                         <Form.Select className="input-box"
                             onChange={(e) => setQuestion3(e.target.value)} value={question3}>
                             <option></option>
-                            <option value="1">Male</option>
-                            <option value="2">Female</option>
-                            <option value="2">I do not wish to self-identify</option>
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                            <option value="No Answer">I do not wish to self-identify</option>
                         </Form.Select>
                     </Form.Group>
                     <br />
@@ -112,10 +112,10 @@ function Survey() {
                         <Form.Select className="input-box"
                             onChange={(e) => setQuestion5(e.target.value)} value={question5} >
                             <option></option>
-                            <option value="1">0-1 hours</option>
-                            <option value="2">2-4 hours</option>
-                            <option value="2">4-6 hours</option>
-                            <option value="2">6+ hours</option>
+                            <option value="0-1 hours">0-1 hours</option>
+                            <option value="2-4 hours">2-4 hours</option>
+                            <option value="4-6 hours">4-6 hours</option>
+                            <option value="6+ hours">6+ hours</option>
                         </Form.Select>
                     </Form.Group>
                     <br />
@@ -129,11 +129,11 @@ function Survey() {
                         <Form.Select className="input-box"
                             onChange={(e) => setQuestion6(e.target.value)} value={question6}>
                             <option></option>
-                            <option value="1">Shut up and take my money</option>
-                            <option value="2">Likely</option>
-                            <option value="2">On The Fence</option>
-                            <option value="2">Not Convinced</option>
-                            <option value="2">Never</option>
+                            <option value="most likely">Shut up and take my money</option>
+                            <option value="likely">Likely</option>
+                            <option value="maybe">On The Fence</option>
+                            <option value="not likely">Not Convinced</option>
+                            <option value="never">Never</option>
                         </Form.Select>
                     </Form.Group>
                     <br />
@@ -147,19 +147,22 @@ function Survey() {
                         <Form.Select className="input-box"
                             onChange={(e) => setQuestion7(e.target.value)} value={question7}>
                             <option></option>
-                            <option value="1">Yes</option>
-                            <option value="2">No</option>
+                            <option value="Yes">Yes</option>
+                            <option value="No">No</option>
                         </Form.Select>
                     </Form.Group>
                     <br />
                     {/* Question 8 */}
-                    <Form.Group className="form-control">
-                        <Form.Label className="sub-text">
-                            If not, what would be?
-                        </Form.Label>
-                        <Form.Control className="input-box" type="text" required
-                            onChange={(e) => setQuestion8(e.target.value)} value={question8} />
-                    </Form.Group>
+                    {/* if statement allows this question to pop up only if question 7 is a no */}
+                    {question7 === 'No' ? (
+                        <div className="form-control">
+                            <label className="sub-text">
+                                If not, what would be?
+                            </label>
+                            <input className="input-box" type="text" required
+                                onChange={(e) => setQuestion8(e.target.value)} value={question8} />
+                        </div>
+                    ) : null}
                     <br />
 
                     {/*  Question 9 */}
@@ -171,10 +174,10 @@ function Survey() {
                         <Form.Select className="input-box"
                             onChange={(e) => setQuestion9(e.target.value)} value={question9}>
                             <option></option>
-                            <option value="1">Very Likely</option>
-                            <option value="2">Likely</option>
-                            <option value="2">Not Likely</option>
-                            <option value="2">Never</option>
+                            <option value="Very Likely">Very Likely</option>
+                            <option value="Likely">Likely</option>
+                            <option value="Not Likely">Not Likely</option>
+                            <option value="Never">Never</option>
                         </Form.Select>
                     </Form.Group>
                     <br />
