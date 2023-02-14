821adf2373e1ddeac1d85b6f7b470e5cbf891231
MikeSanDev <mjs070594@gmail.com>
2023-02-13 21:54:56 -0600

    thank you page after submitting

--- src/pages/Thanks.js
+++ src/pages/Thanks.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import NavbarComponent from '../components/NavbarComponent'
+import Footer from '../components/Footer'
 
 function Thanks() {
     return (
-        <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}>Thank You For Taking This Survey!</motion.div>
+        <>
+            <NavbarComponent />
+            <hr className='thank-hr' />
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                className="thank-you">
+                <h1>Thank You For Taking This Survey!</h1>
+                <a href="/Home" className="thanks-btn">Home Page</a>
+            </motion.div>
+            <hr className='you-hr' />
+            <Footer />
+        </>
+
     )
 }
 
