7ec3c9cd590a8ed2c0ae8a85eaf2706729f4d244
MikeSanDev <mjs070594@gmail.com>
2023-02-15 22:29:43 -0600

    NewSub page

---
+++ src/pages/NewSub.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+import NavbarComponent from '../components/NavbarComponent'
+import Footer from '../components/Footer'
+
+function NewSub() {
+    return (
+        <>
+            <NavbarComponent />
+            <hr className='thank-hr' />
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                className="thank-you">
+                <h1>Thank You For Subscribing!</h1>
+                <a href="/Home" className="thanks-btn">Home Page</a>
+            </motion.div>
+            <hr className='you-hr' />
+            <Footer />
+        </>
+
+    )
+}
+
+export default NewSub
