c6cd0be9d080a22f805c0ee71f046bbde8c7c798
MikeSanDev <mjs070594@gmail.com>
2023-02-17 01:00:28 -0600

    founder edits

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './footer.css';
 import logo from '../images/logo.png';
-import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
 
 function Footer() {
     return (
@@ -14,40 +14,43 @@ function Footer() {
 
                             <h4>Follow Us On</h4>
                             <div className="icon">
-                                <div className='icon-group'>
+                                {/* <div className='icon-group'>
                                     <FaFacebook className="fa-brands fa-facebook" />
                                     <h6 > Facebook </h6>
                                 </div>
                                 <div className='icon-group'>
                                     <FaTwitter className="fa-brands fa-twitter" />
                                     <h6 > Twitter </h6>
-                                </div>
+                                </div> */}
                                 <div className='icon-group'>
-                                    <FaInstagram className="fa-brands fa-instagram-square" />
-                                    <h6 > Instagram</h6>
+
+                                    <a href='https://www.instagram.com/kineticfootwear_/' target="_blank" rel="noreferrer"> Instagram
+                                        <FaInstagram className="fa-brands fa-instagram-square" />
+                                    </a>
+
+                                    <a href="/About">About Us</a>
                                 </div>
                             </div>
+
                         </div>
                         <div className="col-2">
-                            <h4>About</h4>
-                            <a href="/About">About Us</a>
-                            <h6 >Contact Us</h6>
+                            <div className="call-action">
+                                <p className="news-text">
+                                    Stay updated on our journey!
+                                </p>
+                                <form action="https://formsubmit.co/njohn@hawk.iit.edu" method="POST" className='call-action'>
+                                    <input className="email_form" type="email" placeholder="Your Email" name="email" required />
+                                    <input type="hidden" name="_next" value="/Thanks"></input>
+                                    <button className="normal" type="submit">Subscribe</button>
+                                </form>
+                            </div>
                         </div>
-                        <div className="col-3">
+                        {/* <div className="col-3">
                             <h4>My Account</h4>
                             <h6 >Help</h6>
-                        </div>
+                        </div> */}
                     </div>
-                    <div className="call-action">
-                        <p className="news-text">
-                            Stay updated on our journey!
-                        </p>
-                        <form action="https://formsubmit.co/njohn@hawk.iit.edu" method="POST" className='call-action'>
-                            <input className="email_form" type="email" placeholder="Your Email" name="email" required />
-                            <input type="hidden" name="_next" value="/Thanks"></input>
-                            <button className="normal" type="submit">Subscribe</button>
-                        </form>
-                    </div>
+
                 </div>
             </footer>
         </>
--- src/pages/About.js
+++ src/pages/About.js
@@ -17,34 +17,53 @@ function About() {
             <section className="about-main ">
                 <motion.h1
                     transition={{
-                        duration: 0.75
+                        duration: 1.5
                     }}
                     initial={{
-                        x: 20,
+                        y: -20,
                         opacity: 0,
                     }}
                     animate={{
-                        x: 0,
+                        y: 0,
                         opacity: 1,
-                    }} className="tech_title">How It Works:</motion.h1>
-                <div className="main_block">
+                    }} className="tech_title">Our Story:</motion.h1>
+                <div className="tech_info story_info">
+                    <motion.p
+                        transition={{
+                            duration: 2
+                        }}
+                        initial={{
+                            y: -20,
+                            opacity: 0,
+                        }}
+                        animate={{
+                            y: 20,
+                            opacity: 1,
+                        }}>As people who have experienced the frustration of a dead phone at the most inconvenient times, We understand how important it is to stay connected. Getting lost in a new city, missing out on a concert, or not being able to receive an important call can be incredibly stressful. That's why, at Kinetic Footwear, we've combined our passion for shoe design with our engineering expertise to address this growing problem in our increasingly connected world. Our energy harvesting shoe allows you to stay connected on the go without worrying about your phone dying. Join us in our mission to keep you moving, no matter where life takes you.</motion.p></div>
+                <motion.div
+                    transition={{
+                        duration: 2.5
+                    }}
+                    initial={{
+                        y: -20,
+                        opacity: 0,
+                    }}
+                    animate={{
+                        y: 20,
+                        opacity: 1,
+                    }} className="main_block">
                     <div className="tech_block tb1">
                         <div className="tech_photo tech_pic1"><img src={zombie} alt="zombie" />
                         </div>
                         <div className="tech_info">
                             <h5>Walking is an everyday activity!</h5>
-                            <p>When humans walk, they actually put a fair amount of energy into the surface of which they are
-                                moving on. After looking at many studies we found the range to be 50-67 Watts based on specific
-                                variables (weight, speed, etc.). Now this isn’t enough power to power streetlights or buildings
-                                but it is enough to be useful in our daily lives. </p>
+                            <p>Did you know that when you walk, your footsteps generate a significant amount of energy that is absorbed by the surface beneath your feet? While this energy may not be enough to power large buildings or streetlights, it can certainly be harnessed to power smaller devices in our daily lives </p>
                         </div>
                     </div>
                     <div className="tech_block tb2">
                         <div className="tech_info">
                             <h5>Our product does just that.</h5>
-                            <p> Although many people have tried making an energy harvesting shoe before (as demonstrated by the
-                                hundreds of patents on these types of devices) there isn’t a product like ours on the market!
-                                This isn’t because of manufacturing or market reasons, but simply because no one has <span><strong>gotten enough power.</strong></span>
+                            <p> Despite the fact that many attempts have been made to create energy harvesting shoes (as evidenced by the hundreds of patents on such devices), there isn't a product quite like ours on the market today. While other attempts have fallen short due to insufficient power output, our innovative design overcomes this challenge, providing ample power to keep you connected on the go.
                             </p>
                         </div>
                         <div className="tech_photo tech_pic2"><img src={walkRight} alt="walking" />
@@ -54,16 +73,13 @@ function About() {
                         <div className="tech_photo tech_pic1"><img src={jumpLeft} alt="jumping" />
                         </div>
                         <div className="tech_info">
-                            <h5>Our energy harvesting system gets way more energy than anyone else has ever gotten before.</h5>
+                            <h5>How does it work?
+                            </h5>
                             <p>
-                                Enough to make a product that can be useful! We are aiming to allow people to power their lives
-                                by doing nothing more than what they would normally do. Imagine never having to check the
-                                battery on your phone when you leave for work in the morning. Never having to carry around a
-                                bulky power brick and a cable just so you can wait in line for that concert. All of these and
-                                more become possible. </p>
+                                Our energy harvesting shoe is designed to harness the compression energy created by every step and convert it into electricity that can be used to power your portable devices. With our shoe, you can get a full phone's worth of charge in just an hour of walking, making it easier to stay connected and enjoy your activities without worrying about battery life. </p>
                         </div>
                     </div>
-                </div>
+                </motion.div>
             </section>
             <hr classNameName='light-dark-hr' />
             <Footer />
